b2b610738c47a5166c086ca31a9f111e39ebc6e3
Norberto Martínez <norbux@live.com>
2021-12-19 03:18:06 -0600

    -

--- codegen/config/config.go
+++ codegen/config/config.go
@@ -525,7 +525,7 @@ func (c *Config) autobind() error {
 	// Get the models path from the config file
 	fmt.Printf("\nc.Model.Filename : %v\n", c.Model.Filename)
 	//modelsFilePath := c.Model.Filename
-	modelsFilePath := strings.Replace(c.Model.Filename, "models_gen.go", "_tmp_gqlgen_init.go", 1)
+	//modelsFilePath := strings.Replace(c.Model.Filename, "models_gen.go", "_tmp_gqlgen_init.go", 1)
 	//modelsFilePath += "/_tmp_gqlgen_init.go" 
 
 	// Create a temporary file with "package models" to avoid autobind conflicts
